f3e6492cb283c850f0a3f950e8c222d28788bbfe
802-web <ILhs1w7Yo@gmail.com>
2026-09-19 18:36:33 +0900

    enum

---
+++ src/enum.ts
@@ -0,0 +1,36 @@
+export {};
+enum Months {
+  January = 1,
+  Febuary,
+  March,
+  April,
+  May,
+  June,
+  July,
+  August,
+  September,
+  Octorber,
+  November,
+  December,
+}
+
+console.log(Months.April);
+
+enum COLORS {
+  RED = '#FF0000',
+  WHITE = '#FFFFFF',
+  GREEN = '#008000',
+  BRUE = '#0000FF',
+  BALCK = '#000000',
+}
+
+let green = COLORS.GREEN;
+console.log(green);
+
+enum COLORS {
+  YELLOW = '#FFFF00',
+  GRAY = '#808080',
+}
+
+let yellow = COLORS.YELLOW;
+let gray = COLORS.GRAY;
