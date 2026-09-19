9ff8a5fb984789fd1782a8626ec35c5e61cb21a7
802-web <ILhs1w7Yo@gmail.com>
2026-09-19 17:28:59 +0900

    interface

---
+++ src/interface.ts
@@ -0,0 +1,16 @@
+export {};
+
+type Objecttype = {
+  name: string;
+  age: number;
+};
+
+interface ObjectInterface {
+  name: string;
+  age: number;
+}
+
+let object: ObjectInterface = {
+  name: 'hh',
+  age: 2,
+};
