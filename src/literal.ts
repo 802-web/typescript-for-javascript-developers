d57801af3126ef22cf5ab6fb59acc1e12c8b675d
802-web <ILhs1w7Yo@gmail.com>
2026-09-19 18:09:53 +0900

    literal

---
+++ src/literal.ts
@@ -0,0 +1,16 @@
+export {};
+
+let dayOfTheWeek:
+  | 'sun'
+  | 'mon'
+  | 'tue'
+  | 'tue'
+  | 'wen'
+  | 'thu'
+  | 'fri'
+  | 'stu' = 'sun';
+dayOfTheWeek = 'mon';
+
+let month = 1;
+
+let TRUE: true = true;
