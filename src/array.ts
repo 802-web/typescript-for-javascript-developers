2927f38d495022fbf8f640e271fa6d3b066ee908
802-web <ILhs1w7Yo@gmail.com>
2026-09-19 14:40:03 +0900

    array

---
+++ src/array.ts
@@ -0,0 +1,10 @@
+export {};
+let numbers: number[] = [1, 2, 3];
+let string: string[] = ['aa', 'gg', 'dd'];
+let number2: number[][] = [
+  [1, 2],
+  [3, 4],
+  [5, 6],
+  [7, 8],
+];
+console.log(number2);
